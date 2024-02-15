fadd088b8ef110ef8f7f7de4abe0c5489960fd7c
GoPaMiguel <miguel.gonzalezpacheco@unicolombo.edu.co>
2024-02-14 20:13:25 -0500

    Primer Ejercicio

---
+++ Ejercicio1.java
@@ -0,0 +1,9 @@
+// Programa que muestre en pantalla el texto “Fundamentos de Programación”.
+
+public class Ejercicio1 {
+
+    public static void main(String args[]){
+        System.out.println("Fundamentos de programación");
+    }
+
+}
