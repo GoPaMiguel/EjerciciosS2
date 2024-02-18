01c88d4ab286067b6d161dfb8555affe09f93891
GoPaMiguel <miguel.gonzalezpacheco@unicolombo.edu.co>
2024-02-18 17:20:47 -0500

    Ejercicio 18

---
+++ Ejercicio18.java
@@ -0,0 +1,11 @@
+/*
+ * 18.Calcular y escribir el cuadrado de 125.
+ */
+
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        System.out.println("Calcula el cuadrado de 125");
+        double cuadrado = Math.pow(125, 2);
+        System.out.println("EL cuadrado de 125 es: " + cuadrado);
+    }
+}
