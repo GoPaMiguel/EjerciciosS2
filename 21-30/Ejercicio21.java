d8e67fe40a454aefd7f44e8bb744302970b11260
GoPaMiguel <miguel.gonzalezpacheco@unicolombo.edu.co>
2024-02-24 22:44:11 -0500

    Ejercicio 21

---
+++ 21-30/Ejercicio21.java
@@ -0,0 +1,18 @@
+/*
+ * 21.Intercambiar los valores de dos variables A y B.
+ */
+
+public class Ejercicio21 {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        System.out.println("a = " + a + " AND  b = " + b);
+        String c = "a";
+        a = b;
+        b = c;
+        System.out.println("a = " + a + " AND  b = " + b);
+        
+
+
+    }
+}
